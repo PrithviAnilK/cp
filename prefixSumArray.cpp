01fa4c21583f807c0f8c284fa24a3bfb94a50a7c
Prithvi Anil Kumar <thepirateking3d2y@gmail.com>
2020-01-20 23:55:42 +0530

    Added prefixSumArray Template

---
+++ prefixSumArray.cpp
@@ -0,0 +1,24 @@
+vector<int> createPrefixArray(vector<int> &arr)
+{
+    /*
+        Used to create a prefix array from array h,
+        Parameters:- the array from which the prefix array is to be made,
+        Returns prefix array.
+    */
+    int n = arr.size();
+    vector<int> pref(n);
+    pref[0] = arr[0];
+    for(int i = 1;i < n;i++) pref[i] = pref[i-1] + arr[i];
+    return pref;
+}
+
+int sumLR(int l, int r, vector<int> &pref)
+{
+    /*
+        Computes the sum from index 'l' to 'r' in O(1).
+        Parameters:- index l and r, prefix array
+        Returns sum from index l to r.
+    */
+    if(l == 0) return pref[r];
+    return pref[r] - pref[l-1];
+}
