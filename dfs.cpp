764ca1bfb34fe42cff646f4baf477be3a8f84fad
Prithvi Anil Kumar <thepirateking3d2y@gmail.com>
2020-01-25 20:23:51 +0530

    Added makeGraph() function

--- dfs.cpp
+++ dfs.cpp
@@ -14,8 +14,19 @@ void dfs(int node)
     }
 }
 
-void makeGraph(int n)
+/* 
+    Takes arguments number of nodes, number of edges,
+    Makes a graph.
+*/
+void makeGraph(int n, int e)
 {
     fill(g, g+n+1, vector<int>(0));
     fill(vis.begin(), vis.end(), false);
+    for(int i = 0;i < e;i++)
+    {
+        int node1, node2;
+        cin >> node1 >> node2;
+        g[node1].push_back(node2);
+        g[node2].push_back(node1);
+    }
 }
