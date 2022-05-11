966b6ed0add261c6fdadeeed6d6fda26d2761e2a
prithvianilk <thepirateking3d2y@gmail.com>
2022-05-12 00:06:45 +0530

    added diagonal trick for adding all numbers in matrix within the same diagonal

---
+++ diagonal.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <functional>
+
+using namespace std;
+using ll = long long;
+
+template <typename Op = plus<int>, typename InvOp = minus<int>>
+class Diagonal
+{
+private:
+    unordered_map<int, int> diagonal_1, diagonal_2;
+    vector<vector<int>> matrix;
+
+public:
+    Diagonal(const vector<vector<int>> &matrix, Op op = plus<int>())
+    {
+        this->matrix = matrix;
+        int n = matrix.size(), m = matrix[0].size();
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < m; ++j)
+            {
+                diagonal_1[i - j] = op(diagonal_1[i - j], matrix[i][j]);
+                diagonal_2[i + j] = op(diagonal_2[i + j], matrix[i][j]);
+            }
+        }
+    }
+
+    int get_X(int i, int j, Op op = plus<int>(), InvOp invOp = minus<int>()) 
+    {
+        return invOp(op(diagonal_1[i - j], diagonal_2[i + j]), matrix[i][j]);
+    }
+};
+
+int main()
+{
+    vector<vector<int>> v = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    Diagonal diagonal(v);
+    cout << diagonal.get_X(1, 1) << '\n';
+    return 0;
+}
