fb4eaad80e3b6c1a9d693bbbd5d7543a89c87a88
prithvianilk <thepirateking3d2y@gmail.com>
2022-06-24 00:17:46 +0530

    Add both BIT use case implementations
    - Point update, Range Query
    - Range update, Point Query

--- bit.cc
+++ bit.cc
@@ -1,12 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 
+// range update
+// point query
 class bit
 {
 private:
     int n_;
     vector<int> t;
+
+    void update(int idx, int val)
+    {
+        for (; idx < n_; idx = idx | (idx + 1)) {
+            t[idx] += val;
+        }
+    }
 
 public:
     bit(int n) : n_(n), t(vector<int>(n, 0)) {}
@@ -18,18 +26,29 @@ public:
         }
     }
 
-    void update(int idx, int val)
-    {
-        for (; idx < n_; idx = idx | (idx + 1)) {
-            t[idx] += val;
-        }
-    }
-
     void update(int l, int r, int val)
     {
         update(l, val);
         update(r + 1, -val);
     }
+
+    int get(int idx)
+    {
+        int ans = 0;
+        for (; idx >= 0; idx = (idx & (idx + 1)) - 1) {
+            ans += t[idx];
+        }
+        return ans;
+    }
+};
+
+// range query
+// point update
+class bit
+{
+private:
+    int n_;
+    vector<int> t;
 
     int sum(int idx)
     {
@@ -38,5 +57,30 @@ public:
             ans += t[idx];
         }
         return ans;
+    }
+
+public:
+    bit(int n) : n_(n), t(vector<int>(n, 0)) {}
+
+    bit(vector<int> a) : bit(a.size())
+    {
+        for (int i = 0; i < n_; ++i) {
+            update(i, a[i]);
+        }
+    }
+
+    void update(int idx, int val)
+    {
+        for (; idx < n_; idx = idx | (idx + 1)) {
+            t[idx] += val;
+        }
+    }
+
+    int sum(int l, int r)
+    {
+        if (l == 0) {
+            return sum(r);
+        }
+        return sum(r) - sum(l - 1);
     }
 };
